1247271f8112025e69d71d1c9eb7b3bdc0e14889
Colin Brown <colindonovanbrown@gmail.com>
2018-07-06 14:45:20 -0400

    Add basic carousel to lists

--- src/components/List.js
+++ src/components/List.js
@@ -5,10 +5,40 @@ class List extends React.Component {
 
     render() {
         const listItems = this.props.calls.map(call => <Call {...call} key={call.name} onClick={() => this.props.onClick(call.name)} />);
+        while (listItems.length % 11 !== 0) {
+            listItems.push(<Call disabled={true} name="~" group={0} key={listItems.length % 11} onClick={() => console.log("pushed")} />)
+        }
+        const id = this.props.id || "listCarousel";
         return (
-            <ul className={`list-group ${this.props.size}`}>
-                {listItems}
-            </ul>
+
+            <div id={id} className={`carousel slide ${this.props.size}`} data-wrap="false" data-interval="false">
+                <div className="col-md-1"></div>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                    <ul className={`list-group`}>
+                        {listItems}
+                    </ul>
+                    </div>
+                    <div className="carousel-item">
+                    <ul className={`list-group`}>
+                        {listItems}
+                    </ul>
+                    </div>
+                    <div className="carousel-item">
+                    <ul className={`list-group`}>
+                        {listItems}
+                    </ul>
+                    </div>
+                </div>
+                <a className="carousel-control-prev btn" href={`#${id}`} role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="btn carousel-control-next" href={`#${id}`} role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
         )
     }
 
