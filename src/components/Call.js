eb6e3966fef7198fc6f94f493c301ed9996771fa
Colin Brown <colindonovanbrown@gmail.com>
2018-07-08 14:54:26 -0400

    Restyles and style toggled/disabled calls

--- src/components/Call.js
+++ src/components/Call.js
@@ -4,17 +4,19 @@ class Call extends React.Component {
 
     render() {
         var styling = "";
+        var dataTarget = "#exportModal";
         if (this.props.empty) {
             styling = "disabled list-group-item-light call-empty";
+            dataTarget = "";
         } else if (this.props.disabled) {
-            styling = "list-group-item-dark";
+            styling = "list-group-item-dark call-disabled";
         } else {
             styling = `btn-outline-light group-${this.props.group}`;
         }
         return (
             <li 
             data-toggle="modal" 
-            data-target="#exportModal" 
+            data-target={dataTarget} 
             className={`btn list-group-flush list-group-item call ${styling} `} 
             onClick={this.props.onClick}>
                 <span className={this.props.empty ? "empty" : ""}>{this.props.name}</span>
