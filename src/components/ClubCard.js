249826e916a0b4dfed698efff3e58c19f33414e5
Colin Brown <colindonovanbrown@gmail.com>
2018-07-27 17:52:41 -0400

    Add stats to clubb card

--- src/components/ClubCard.js
+++ src/components/ClubCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { totalCalls } from "../util/calls";
 
 class ClubCard extends React.Component {
 
@@ -11,8 +12,8 @@ class ClubCard extends React.Component {
               <div className="card club-card box-shadow">
                 <h4 className="club-card-title card-title bg-secondary text-white">{this.props.name}</h4>
                 <div className="card-body d-flex flex-column align-content-end">
-                    <h5>Last active</h5>
-                    <p className="card-text">{created}</p>
+                    <p className="card-text">{this.props.taught} calls out of {totalCalls} taught</p>
+                    <p className="card-text text-secondary">{created}</p>
                     <div className="club-card-button">
                         <NavLink className={`btn btn-info`} to={`/club`} onClick={() => this.props.updateActiveClub(this.props.name)}>Select Club</NavLink>
                     </div>
