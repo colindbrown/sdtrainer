ce3e84cac16628dfbe1181a70fe3be33a56f68fa
Colin Brown <colindonovanbrown@gmail.com>
2019-09-08 19:40:01 -0400

    Make default sort for run view alphabetical

--- src/components/RunSessionView.js
+++ src/components/RunSessionView.js
@@ -11,7 +11,7 @@ class RunSessionView extends React.Component {
         loading: false,
         planNames: [],
         activeSession: "",
-        sort: "userPosition"
+        sort: ""
     }
 
 
